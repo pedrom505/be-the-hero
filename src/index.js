106f16c6f22896270c5963476f42641c0fb62ebd
Jayesh Choudhary <jayesh.choudhary@spaceuptech.com>
2018-01-18 14:45:13 +0530

    added history and store file

--- src/index.js
+++ src/index.js
@@ -3,22 +3,17 @@ import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 
 import { Provider } from 'react-redux'
-import { createStore, applyMiddleware } from 'redux'
-import { composeWithDevTools } from 'redux-devtools-extension';
 
 import App from './App.jsx';
 import './css/app.css'
-import reducers from './reducers'
-import {connect, clientApiMiddleware, notificationMiddleware} from './reducers/client-api'
+import store from './store'
 
-let store = createStore(reducers, applyMiddleware(clientApiMiddleware, notificationMiddleware), composeWithDevTools());
 // store.dispatch(connect('ws://localhost:8080/v1/client/json/socket', true));
 
-
 ReactDOM.render(
-    <Provider store={store}>
+  <Provider store={store}>
     <App />
-    </Provider>, document.getElementById('root')
-  );
+  </Provider>, document.getElementById('root')
+);
 
 registerServiceWorker();
