8ba20e7412f2f7cfcf84a8d2e4b580e5d970ae28
Jayesh Choudhary <jayesh.choudhary@spaceuptech.com>
2018-01-21 12:24:52 +0530

    Minor changes

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,12 +2,6 @@ import { combineReducers } from 'redux'
 import { reducer as notifications } from 'react-notification-system-redux';
 import { createReducer } from './reducers-middleware'
 
-
-const account = combineReducers({
-  users: createReducer('account.users', 'literal-array', []),
-  accounts: createReducer('account.accounts', 'literal-array', []),
-})
-
-const reducers = combineReducers({ notifications, account });
+const reducers = combineReducers({ notifications });
 
 export default reducers;
