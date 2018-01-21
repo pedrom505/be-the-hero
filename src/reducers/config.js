a4bb3d1cbd04fce9878dcb78276c75991d02aa4a
Jayesh Choudhary <jayesh.choudhary@spaceuptech.com>
2018-01-21 12:30:53 +0530

    Minor changes

--- src/reducers/config.js
+++ src/reducers/config.js
@@ -1,19 +1,4 @@
 const config = {
-  "action1-response": [
-    {
-      type: 'account.users',
-      operation: 'append',
-      value: 'data.val7.val2',
-      conditions: [{ operator: '===', operand1: 'data.ack', operand2: true }, { operator: '!==', operand1: 'data.val7.val2', operand2: undefined }]
-    }
-  ],
-  "action2-response": [
-    {
-      type: 'account.users',
-      operation: 'removeByValue',
-      value: 'data.val1',
-      conditions: [{ operator: '===', operand1: 'data.ack', operand2: true }]
-    }
-  ]
+  "action1-response": []
 }
 export default config
