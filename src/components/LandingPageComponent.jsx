01566df74305215322c19a4dcc5a5d1d28ab624f
Jayesh <choudharyjayesh333@gmail.com>
2018-03-12 20:04:45 +0530

    Redirected user to home page if session alive

--- src/components/LandingPageComponent.jsx
+++ src/components/LandingPageComponent.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import history from '../history'
 
 class LandingPageComponent extends React.Component {
   constructor(props) {
     super(props)
+  }
+
+  componentDidMount() {
+    if (document.cookie.length > 0) {
+      history.push('/home')
+    }
   }
 
   render() {
